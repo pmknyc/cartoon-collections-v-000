be0431a9ba6f5f3512736316eb4d551b0512379a
Paula McKinley <pmknyc@optimum.net>
2018-10-15 01:17:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) # call array of names
+  dwarves.each.with_index(1) do |name, i| 
+    puts "#{i}. #{name}" #each name list with 
+  end
 end
 
 def summon_captain_planet# code an argument here
